30c028e5235c60cd9297182774f707f071484508
Andy <amulder964@aol.com>
2020-06-29 13:43:22 +0100

    Used Properties to store the current occupancy in case the OS stops the app

--- Counter/Counter/App.xaml.cs
+++ Counter/Counter/App.xaml.cs
@@ -6,30 +6,45 @@ using Xamarin.Forms.Xaml;
 namespace Counter
 {
     public partial class App : Application
-    {        
+    {
+        // \key used for storing preference
+        const string savedOccupancyCount = "savedOccupancyCount";
+
+        // Holds the current occupancy value for the building
+        public static int occupancyCount { get; set; }        
+
         public App()
         {
             InitializeComponent();
-
-            MainPage = new MainPage();
-            
+            MainPage = new MainPage();            
         }
 
         protected override void OnStart()
         {
-            
+            // TODO Might need to load the occupancyCount here
+            Console.WriteLine("OnResume");
+            if (Properties.ContainsKey(savedOccupancyCount))
+            {
+                occupancyCount = (int)Properties[savedOccupancyCount];
+                Console.WriteLine("Loading occupancyCount value of " + occupancyCount.ToString());
+            }
+            else
+            {
+                occupancyCount = 0;
+            }
         }
 
         protected override void OnSleep()
         {
             // TODO Might need to store the occupancyCount here to persist the value
             // This might mean putting the variable in this file and providing getter setter for MainPage to access
-            
+            Console.WriteLine("OnSleep, saving occupancyCount value of " + occupancyCount.ToString());
+            Properties[savedOccupancyCount] = occupancyCount;
         }
 
         protected override void OnResume()
         {
-            // TODO MIght need to load the occupancyCount here
+            Console.WriteLine("OnResume");
         }
     }
 }
--- Counter/Counter/MainPage.xaml.cs
+++ Counter/Counter/MainPage.xaml.cs
@@ -10,9 +10,7 @@ namespace Counter
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
-    {
-        // Holds the current occupancy value for the building
-        public int occupancyCount;
+    {        
         // Holds the maximum Occupancy value for the building. Ask if this will be fixed or need a way of changing in the app
         private int maximumOccupancy;
         // Hodls the hourly count value
@@ -20,30 +18,36 @@ namespace Counter
 
         public MainPage()
         {
-            InitializeComponent();
-            // Set initial values
-            occupancyCount = 0;
-            currentOccupancyLabel.Text = occupancyCount.ToString();
+            
+            InitializeComponent();            
+            
             maximumOccupancy = 20; 
             maximumOccupancyLabel.Text = "Maximum: " + maximumOccupancy.ToString();
             hourlyCount = 0;
             hourlyCountLabel.Text = "Hourly Count: " + hourlyCount.ToString();
         }
 
+        // Sets the initial values of the counts
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            currentOccupancyLabel.Text = App.occupancyCount.ToString();
+        }
+
         // Event handlers for the + and - buttons so that increase ior decrease the current occupency
         private void Add_Button_Clicked(object sender, EventArgs e)
         {
             // Update the occupancy counter
-            occupancyCount += 1;
-            currentOccupancyLabel.Text = occupancyCount.ToString();
+            App.occupancyCount += 1;
+            currentOccupancyLabel.Text = App.occupancyCount.ToString();
 
             // Check for the count approaching the maximum
-            if (occupancyCount >= maximumOccupancy)
+            if (App.occupancyCount >= maximumOccupancy)
             {
                 currentOccupancyLabel.TextColor = Color.Red;
                 // TODO A reminder could be displayed telling the user maximum occupancy has been reached and wait for people to leave before anyone else is allowed to enter.
             }
-            else if (maximumOccupancy - occupancyCount <= 5)
+            else if (maximumOccupancy - App.occupancyCount <= 5)
             {
                 currentOccupancyLabel.TextColor = Color.Orange;                
             }            
@@ -54,21 +58,21 @@ namespace Counter
         }
         private void Subtract_Button_Clicked(object sender, EventArgs e)
         {
-            occupancyCount -= 1;
-            if (occupancyCount < 0)
+            App.occupancyCount -= 1;
+            if (App.occupancyCount < 0)
             {
-                occupancyCount = 0;
+                App.occupancyCount = 0;
             }
             // Check to see if the occupancy is less than a color warning boundary
-            if (maximumOccupancy - occupancyCount == 6)
+            if (maximumOccupancy - App.occupancyCount == 6)
             {
                 currentOccupancyLabel.TextColor = Color.Default;
             }
-            else if (maximumOccupancy - occupancyCount == 1)
+            else if (maximumOccupancy - App.occupancyCount == 1)
             {
                 currentOccupancyLabel.TextColor = Color.Orange;
             }
-            currentOccupancyLabel.Text = occupancyCount.ToString();
+            currentOccupancyLabel.Text = App.occupancyCount.ToString();
         }
 
         // Resets the hourly count
