0720bacad1118cb851dc9d0847db49b6f9670b7e
Andy <amulder964@aol.com>
2020-07-01 11:32:25 +0100

    App now changes occ count to correct colour after loading a stored value

--- Counter/Counter/MainPage.xaml.cs
+++ Counter/Counter/MainPage.xaml.cs
@@ -12,47 +12,39 @@ namespace Counter
     public partial class MainPage : ContentPage
     {        
         // Holds the maximum Occupancy value for the building. Ask if this will be fixed or need a way of changing in the app
-        private int maximumOccupancy;
-        
+        private const int maximumOccupancy = 20;
+        private const string hourly = "Hourly Count: ";
 
         public MainPage()
-        {            
+        {
             InitializeComponent();            
             
-            maximumOccupancy = 20; 
             maximumOccupancyLabel.Text = "Maximum: " + maximumOccupancy.ToString();            
         }
-
+        
         // Sets the initial values of the counts
         protected override void OnAppearing()
         {
             base.OnAppearing();
+            // An occupancy count and/or hourly count might have been loaded so update the UI
             currentOccupancyLabel.Text = App.occupancyCount.ToString();
-            // TODO Check to see if a font colour change is needed. Make a method
-            hourlyCountLabel.Text = "Hourly Count: " + App.hourlyCount.ToString();
+            CheckForColourChange();
+            hourlyCountLabel.Text = hourly + App.hourlyCount.ToString();
         }
+
+        #region Click Handlers
 
         // Event handlers for the + and - buttons so that increase ior decrease the current occupency
         private void AddButtonClicked(object sender, EventArgs e)
         {
-            // Update the occupancy counter
+            // Update the occupancy counter and check to see if we need to change the colour of the occupancy value
             App.occupancyCount += 1;
             currentOccupancyLabel.Text = App.occupancyCount.ToString();
-
-            // Check for the count approaching the maximum
-            if (App.occupancyCount >= maximumOccupancy)
-            {
-                currentOccupancyLabel.TextColor = Color.Red;
-                // TODO A reminder could be displayed telling the user maximum occupancy has been reached and wait for people to leave before anyone else is allowed to enter.
-            }
-            else if (maximumOccupancy - App.occupancyCount <= 5)
-            {
-                currentOccupancyLabel.TextColor = Color.Orange;                
-            }
+            CheckForColourChange();
 
             // Update the hourly count value
             App.hourlyCount += 1;
-            hourlyCountLabel.Text = "Hourly Count: " + App.hourlyCount.ToString();
+            hourlyCountLabel.Text = hourly + App.hourlyCount.ToString();
         }
         private void SubtractButtonClicked(object sender, EventArgs e)
         {
@@ -79,7 +71,25 @@ namespace Counter
             // Check the user wanted to do this.
             await Navigation.PushModalAsync(new HourlyCountResetPage());   
             // Refresh the hourly count on the UI
-            hourlyCountLabel.Text = "Hourly Count: " + App.hourlyCount.ToString();
+            hourlyCountLabel.Text = hourly + App.hourlyCount.ToString();
+        }
+        #endregion
+
+        /// <summary>
+        /// Checks to see if the Occupancy Count figue on the UI needs to change colour after an increase
+        /// </summary>
+        private void CheckForColourChange()
+        {
+            // Check for the count approaching the maximum
+            if (App.occupancyCount >= maximumOccupancy)
+            {
+                currentOccupancyLabel.TextColor = Color.Red;
+                // TODO A reminder could be displayed telling the user maximum occupancy has been reached and wait for people to leave before anyone else is allowed to enter.
+            }
+            else if (maximumOccupancy - App.occupancyCount <= 5)
+            {
+                currentOccupancyLabel.TextColor = Color.Orange;
+            }
         }
     }
 }
